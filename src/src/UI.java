3fd79f2a3ede68d4c1362a5d3f04cc39307b6dfc
david <lutalidavid44@gmail.com>
2023-02-23 12:45:09 +0300

    23 Feb

---
+++ src/src/UI.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class UI extends JFrame {
+    JFrame frame =new JFrame("EX");
+    JLabel disclaimer = new JLabel("This information will not be used for any unlawful activities");
+
+}
